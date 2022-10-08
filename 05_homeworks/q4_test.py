f63981f17c5b7a3dc174b07325efe009b289475b
DaniTrece <96174415+DaniTrece@users.noreply.github.com>
2022-10-08 22:25:08 +0200

    Add files via upload

---
+++ 05_homeworks/q4_test.py
@@ -0,0 +1,9 @@
+
+import requests
+
+url = "http://127.0.0.1:9696/predict"
+
+client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
+respose = requests.post(url, json=client).json()
+
+print(respose)
